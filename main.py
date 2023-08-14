f35a1681b1557a66801f31dd6549fa2e3f4b17a0
Eren Balcı <eren.balci2111@gmail.com>
2023-08-14 16:07:14 +0300

    image and messagebox revision

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import tkinter
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
 
 window = tkinter.Tk()
 window.title("BMI Calculator")
 window.minsize(width=300, height=200)
 window.config(padx=30, pady=30)
+
+#image part
+my_image = Image.open("img.png")
+resize_my_image = my_image.resize((90, 90))
+my_new_image = ImageTk.PhotoImage(resize_my_image)
+my_label = tkinter.Label(window, image=my_new_image)
+my_label.pack()
 
 #weight part
 weight_label = tkinter.Label(text="Enter Your Weight (kg)")
@@ -23,53 +33,55 @@ height_entry = tkinter.Entry()
 height_entry.config(width=15)
 height_entry.pack()
 
-# calculate button part
+
 def user_information():
-
     try:
-        user_weight = float(weight_entry.get())
-        user_height = float(height_entry.get())
+        if len(weight_entry.get()) == 0 or len(height_entry.get()) == 0:
+            messagebox.showinfo(title="Missing Info", message="Enter all info.")
 
-        bmi = ((user_weight) / (user_height * user_height)) * 10000
+        elif float(weight_entry.get()) < 0 or float(height_entry.get()) < 0:
+            messagebox.showerror(title="ERROR!", message="Weight or height values cannot be negative.")
 
-        if user_weight < 0 or user_height < 0:
-            result_print = "You cannot have negative values."
-
-        elif user_height > 250 or user_weight > 300:
-            result_print = "Please enter real values!."
-
-        elif bmi <= 18.5:
-            result_print = "Your BMI is: %3.1f.\tYou are underweight." % (bmi)
-
-        elif (bmi > 18.5) and (bmi <= 24.9):
-            result_print = "Your BMI is: %3.1f.\tYou are normal." % (bmi)
-
-        elif (bmi >= 25) and (bmi <= 30):
-            result_print = "Your BMI is: %3.1f.\tYou are overweight." % (bmi)
-
-        elif (bmi >= 30) and (bmi <= 34.9):
-            result_print = "Your BMI is: %3.1f.\tYou are Obese (class 1)." % (bmi)
-
-        elif (bmi >= 35) and (bmi <= 39.9):
-            result_print = "Your BMI is: %3.1f.\tYou are Obese (class 2)." % (bmi)
-
-        elif bmi >= 40:
-            result_print = "Your BMI is: %3.1f.\tYou are obese (class 3)." % (bmi)
+        elif float(weight_entry.get()) > 250 or float(height_entry.get()) > 250:
+            messagebox.showerror(title="ERROR!", message="Enter real values.")
 
         else:
-            result_print = "You are not in BMI interval for diagnosis"
+            user_weight = float(weight_entry.get())
+            user_height = float(height_entry.get())
+            bmi = (user_weight / (user_height * user_height)) * 10000
+
+            if bmi <= 18.5:
+                result_print = "Your BMI is: %3.1f.\tYou are underweight." % (bmi)
+
+            elif (bmi > 18.5) and (bmi <= 24.9):
+                result_print = "Your BMI is: %3.1f.\tYou are normal." % (bmi)
+
+            elif (bmi >= 25) and (bmi <= 30):
+                result_print = "Your BMI is: %3.1f.\tYou are overweight." % (bmi)
+
+            elif (bmi >= 30) and (bmi <= 34.9):
+                result_print = "Your BMI is: %3.1f.\tYou are Obese (class 1)." % (bmi)
+
+            elif (bmi >= 35) and (bmi <= 39.9):
+                result_print = "Your BMI is: %3.1f.\tYou are Obese (class 2)." % (bmi)
+
+            elif bmi >= 40:
+                result_print = "Your BMI is: %3.1f.\tYou are obese (class 3)." % (bmi)
+
+            else:
+                result_print = "You are not in BMI interval for diagnosis"
+
+
+            result = tkinter.Label(text=result_print, font=("Arial", 11, "bold"))
+            result.config(padx=10, pady=10)
+            result.pack()
 
     except:
-        result_print = "Enter valid numbers!"
+        messagebox.showerror(title="ERROR", message="Enter valid numbers.")
 
-    result = tkinter.Label(text=result_print, font=("Arial", 11, "bold"))
-    result.config(padx=10, pady=10)
-    result.pack()
+
 
 calculate_button = tkinter.Button(text="Calculate", command=user_information)
 calculate_button.pack()
-
-
-
 
 window.mainloop()
